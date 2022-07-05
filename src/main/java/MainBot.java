8151eb247fced6150baca566fe9cb142e4ed46d5
AndrewChmutov <ulman.andre@gmail.com>
2022-07-05 21:25:46 +0200

    Adding command response and register bot

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -1,11 +1,18 @@
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -17,11 +24,15 @@ public class MainBot extends TelegramLongPollingBot{
 	{
 		if(update.hasMessage())
 		{
-			handleMessage(update.getMessage());
+			try {
+				handleMessage(update.getMessage());
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	public void handleMessage(Message message)
+	public void handleMessage(Message message) throws TelegramApiException
 	{
 		if (message.hasText() && message.hasEntities())
 		{
@@ -29,7 +40,20 @@ public class MainBot extends TelegramLongPollingBot{
 					message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
 			if(commandEntity.isPresent())
 			{
+				String command = message.getText().substring(
+						commandEntity.get().getOffset(), commandEntity.get().getLength());
 				
+				switch(command)
+				{
+					case "/choose_object":
+//						List<List<InlineKeyboardButton>> buttons= new ArrayList<>();
+//						buttons.add(
+//								 
+//						)
+						
+						execute(SendMessage.builder().text("Choose type of stalker::").chatId(message.getChatId().toString()).build());
+				}
+				return;
 			}
 		}
 	}
@@ -48,6 +72,7 @@ public class MainBot extends TelegramLongPollingBot{
 	{
 		MainBot bot = new MainBot();
 		//bot.execute(SendMessage.builder().chatId("597587192").text("Hello world!").build());
-		
+		TelegramBotsApi tba = new TelegramBotsApi(DefaultBotSession.class);
+		tba.registerBot(bot);
 	}
 }
