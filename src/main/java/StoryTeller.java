b44a192b81c3703cf1cd8797fc4091304e1cabfd
AndrewChmutov <ulman.andre@gmail.com>
2022-07-06 00:50:56 +0200

    Continue creating StoryTeller

--- src/main/java/StoryTeller.java
+++ src/main/java/StoryTeller.java
@@ -1,4 +1,14 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class StoryTeller {
-
+	public static String returnJoke(String joke)
+	{
+		
+		File file = null;
+		Scanner scanner = null;
+		
+				
+	}
 }
